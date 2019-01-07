17bef16757d071686c2463642ad4fb0e1f5c672d
hightouch <hightouch@hotmail.fr>
2019-01-07 18:36:51 +0100

    up

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ http.listen(port, function () {
     console.log('Server Started. Listening on *:' + port);
 });
 
-var chatters = [];
-var chat_messages = [];
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -25,61 +23,10 @@ app.get('/', function (req, res) {
     res.json({ hello: 'world' });
 });
 
-app.post('/join', function (req, res) {
-    var username = req.body.username;
-    if (chatters.indexOf(username) === -1) {
-        chatters.push(username);
-        res.send({
-            'chatters': chatters,
-            'status': 'OK'
-        });
-    } else {
-        res.send({
-            'status': 'FAILED'
-        });
-    }
-});
-
-app.post('/leave', function (req, res) {
-    var username = req.body.username;
-    chatters.splice(chatters.indexOf(username), 1);
-    res.send({
-        'status': 'OK'
-    });
-});
-
-app.post('/send_message', function (req, res) {
-    var room = req.body.room;
-    var username = req.body.username;
-    var message = req.body.message;
-    var date = new Date().toUTCString()
-    chat_messages.push({
-        'room': room,
-        'sender': username,
-        'message': message,
-        'date': date
-    });
-    res.send({
-        'status': 'OK'
-    });
-});
-
-app.get('/get_messages', function (req, res) {
-    res.send(chat_messages);
-});
-
-app.get('/get_chatters', function (req, res) {
-    res.send(chatters);
-});
-
 io.on('connection', function (socket) {
     socket.on('message', function (data) {
         io.emit('send', data);
     });
-    socket.on('update_chatter_count', function (data) {
-        io.emit('count_chatters', data);
-    });
-
 });
 
 
@@ -97,6 +44,7 @@ stream.on("data", function (block) {
             if (object[i].operations[0][0] === "transfer") {
                 var op = object[i].operations[0][1]
                 var block = object[i].block_num
+                op.project= {}
                 console.log(op)
                 io.emit('send', op);
                 if (op.memo.includes('Fundition-') || op.memo.includes('fundition-') || op.memo.includes('Project=Fundition-') && op.to != 'smartmarket') {
@@ -109,18 +57,7 @@ stream.on("data", function (block) {
                         else {
                             var name = 'anonymous'
                         }
-                        // if (op.from === "blocktrades") {
-                        //     WriteDonation(block, name, op, memo)
-                        // }
-                        // if
-                        //     (op.from === "fundition") {
-                        //     WriteDonation(block, name, op, memo)
-                        // }
-                        // else {
-                        //     WriteDonation(block, name, op, memo)
-                        // }
                     }
-
                 }
             }
         }
