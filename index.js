750773de53e7a437fce25a8cfd5933ab89c4ae5b
hightouch <hightouch@hotmail.fr>
2019-01-07 17:43:46 +0100

    up

--- index.js
+++ index.js
@@ -3,28 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-var redis = require('redis');
-var client = '';
-client = redis.createClient(process.env.REDIS_URL);
-
-client.once('ready', function () {
-
-    // Flush Redis DB
-    //client.flushdb();
-    client.get('chat_users', function (err, reply) {
-        if (reply) {
-            chatters = JSON.parse(reply);
-        }
-    });
-
-    client.get('chat_app_messages', function (err, reply) {
-        if (reply) {
-            messages = JSON.parse(reply)
-            chat_messages = messages.slice(messages.length-20,messages.length)
-        }
-    });
-});
+const { Client, BlockchainMode } = require('dsteem');
 
 var port = process.env.PORT || 8080;
 
@@ -51,62 +30,56 @@ app.get('/', function (req, res) {
     res.json({ hello: 'world' });
 });
 
-app.post('/join', function (req, res) {
-    var username = req.body.username;
-    if (chatters.indexOf(username) === -1) {
-        chatters.push(username);
-        client.set('chat_users', JSON.stringify(chatters));
-        res.send({
-            'chatters': chatters,
-            'status': 'OK'
-        });
-    } else {
-        res.send({
-            'status': 'FAILED'
-        });
+var stream = client.blockchain.getBlockStream({ mode: BlockchainMode.Latest })
+stream.on("data", function (block) {
+    if (block != null) {
+        try {
+            var object = JSON.stringify(block.transactions)
+            object.replace("\\", "")
+            object = JSON.parse(object)
+        } catch (error) {
+            console.log(error)
+        }
+        for (i = 0; i < object.length; i++) {
+            if (object[i].operations[0][0] === "transfer") {
+                var op = object[i].operations[0][1]
+                var block = object[i].block_num
+                io.emit('send', op);
+                if (op.memo.includes('Fundition-') || op.memo.includes('fundition-') || op.memo.includes('Project=Fundition-') && op.to != 'smartmarket') {
+                    console.log('this is a donation from ' + op.from)
+                    op.memo = op.memo.replace("/", "°")
+                    if (op.memo) {
+                        var memo = op.memo.split(" ")
+                        if (memo[1].split('=')[1])
+                            var name = memo[1].split('=')[1]
+                        else {
+                            var name = 'anonymous'
+                        }
+                        // if (op.from === "blocktrades") {
+                        //     WriteDonation(block, name, op, memo)
+                        // }
+                        // if
+                        //     (op.from === "fundition") {
+                        //     WriteDonation(block, name, op, memo)
+                        // }
+                        // else {
+                        //     WriteDonation(block, name, op, memo)
+                        // }
+                    }
+
+                }
+            }
+        }
     }
-});
-
-app.post('/leave', function (req, res) {
-    var username = req.body.username;
-    chatters.splice(chatters.indexOf(username), 1);
-    client.set('chat_users', JSON.stringify(chatters));
-    res.send({
-        'status': 'OK'
+})
+    .on('end', function () {
+        // done
+        console.log('END');
     });
-});
 
-app.post('/send_message', function (req, res) {
-    var room = req.body.room;
-    var username = req.body.username;
-    var message = req.body.message;
-    var date = new Date().toUTCString()
-    chat_messages.push({
-        'room': room,
-        'sender': username,
-        'message': message,
-        'date': date
-    });
-    client.set('chat_app_messages', JSON.stringify(chat_messages));
-    res.send({
-        'status': 'OK'
-    });
-});
-
-app.get('/get_messages', function (req, res) {
-    res.send(chat_messages);
-});
-
-app.get('/get_chatters', function (req, res) {
-    res.send(chatters);
-});
 
 io.on('connection', function (socket) {
     socket.on('message', function (data) {
         io.emit('send', data);
     });
-    socket.on('update_chatter_count', function (data) {
-        io.emit('count_chatters', data);
-    });
-
 });
